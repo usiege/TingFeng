c725887211a0e4b47bf95884d44288fc47e6eac8
usiege <jianin45@qq.com>
2020-07-28 00:16:53 +0800

    Create learn_lua.lua

---
+++ Codes/learn_lua.lua
@@ -0,0 +1,18 @@
+-- 欢迎来到听风的艾泽拉斯日常
+print("Hello Azeroth!")
+ChatFrame3:AddMessage("你好，艾泽拉斯！")
+DEFAULT_CHAT_FRAME:AddMessage("你好，艾泽拉斯！")
+
+-- type
+print(type("hello azeroth"))
+print(type(10.4))
+print(type(print))
+print(type(type))
+print(type(true))
+print(type(nil))
+
+--[[
+一行最好只写一句话，如果一行有多个语句的情况下用分号和逗号分开
+--]]
+-- 表达式
+
