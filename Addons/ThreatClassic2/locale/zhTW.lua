aa6c96945dd7716409f1f68e938e6cffc776e9ec
usiege <jianin45@qq.com>
2020-07-27 18:09:16 +0800

    rm addons

--- Addons/ThreatClassic2/locale/zhTW.lua
+++
@@ -1,8 +0,0 @@
-local TC2, C, L, _ = unpack(select(2, ...))
-if TC2.locale ~= "zhTW" then return end
-
------------------------------
---	zhTW client
------------------------------
--- main frame
-L.gui_threat		= "威脅"
